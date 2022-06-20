c389084e320ce7963a93272aaa85629b8b29c2ed
kamil.jarosz <s5106502@bournemouth.ac.uk>
2022-06-20 19:49:06 +0200

    QA instance

--- app/src/main/java/com/paweljablonski/summary/model/MSurvey.kt
+++ app/src/main/java/com/paweljablonski/summary/model/MSurvey.kt
@@ -2,7 +2,7 @@ package com.paweljablonski.summary.model
 
 data class MSurvey(
     var id: String? = null,
-    var employee: MUser? = null,
+    var employee: MUser? = null, //
     var evaluator: MUser? = null, //set by currentLogged user
     var comment: String? = null,
 )
