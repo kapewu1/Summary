e0205efc9bf5a8d2819a461285095bd68cb8dfd5
kamil.jarosz <s5106502@bournemouth.ac.uk>
2022-07-09 13:15:42 +0200

    Backup

--- app/src/main/java/com/paweljablonski/summary/model/MSurvey.kt
+++ app/src/main/java/com/paweljablonski/summary/model/MSurvey.kt
@@ -2,7 +2,7 @@ package com.paweljablonski.summary.model
 
 data class MSurvey(
     var id: String? = null,
-    var employee: MUser? = null, //
+    var employee: MUser? = null, //dd
     var evaluator: MUser? = null, //set by currentLogged user
     var comment: String? = null,
     var questions: List<MQuestion>? = null
